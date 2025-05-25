142df7f08fc400879239a34609a21317ec5b13f9
Evgeny Danilov <jkuujhx2@gmail.com>
2025-05-25 13:30:40 +0700

    Refactor: rewrite test_masks.py using parametrize style

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,9 +1,18 @@
-from masks import get_mask_card_number, get_mask_account
+﻿import pytest
+from src.masks import get_mask_card_number, get_mask_account
 
+@pytest.mark.parametrize('card_num, expected', [
+    ('1234567890123456', '1234 56** **** 3456'),
+    ('5555444433332222', '5555 44** **** 2222'),
+    ('1111222233334444', '1111 22** **** 4444')
+])
+def test_get_mask_card_number(card_num, expected):
+    assert get_mask_card_number(card_num) == expected
 
-def test_get_mask_card_number():
-    assert get_mask_card_number("1234567890123456") == "1234 56** **** 3456"
-
-
-def test_get_mask_account():
-    assert get_mask_account("123456789012") == "**9012"
+@pytest.mark.parametrize('account_num, expected', [
+    ('123456789012', '**9012'),
+    ('9876543210987654', '**7654'),
+    ('0000000000009999', '**9999')
+])
+def test_get_mask_account(account_num, expected):
+    assert get_mask_account(account_num) == expected
